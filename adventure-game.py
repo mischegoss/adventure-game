96b06fb26db4d538a86cf14c28ecb86a3de412d2
Tamar <mischegoss@gmail.com>
2020-03-27 01:20:22 -0400

    Version 1 for submission

--- adventure-game.py
+++ adventure-game.py
@@ -1,36 +1,56 @@
 import time
-import random 
+import random
 
 
 # This is the print_pause helper function as in the instructions
+
 
 def print_pause(my_string):
     print(my_string)
     time.sleep(2)
 
+# I want the choices to be random in the intro
 
-
-#I want the choices to be random in the intro
 
 def play_game():
-    option1 = random.choice(["red box", "blue box", "basket of puppies", "golden box with no key",
-                            "very old book", "Marauder's map", "tiny black box", "jar of something foul but glimmering"])
-    option2 = random.choice(["old coffee can", "purple box with feathers", "invitation from a witch", "mysterious envelope",
-                            "very old pirate's map", "box that says 'do not open'", "magical pumpkin", "a bag marked with skull and cross bones"])
+    option1 = random.choice(["red box",
+                             "blue box",
+                             "basket of puppies",
+                             "golden box with no key",
+                             "very old book",
+                             "Marauder's map",
+                             "tiny black box",
+                             "jar of something foul but glimmering"])
+
+    option2 = random.choice(["old coffee can",
+                             "purple box with feathers",
+                             "invitation from a witch",
+                             "mysterious envelope",
+                             "very old pirate's map",
+                             "box that says 'do not open'",
+                             "magical pumpkin",
+                             "a bag marked with skull and cross bones"])
+
     intro(option1, option2)
-    
+
 
 def intro(option1, option2):
-    print_pause("You wake up one morning and find you had an unexpected and unseen visitor.")   
-    print_pause("On your desk there are two mysterious things. \nOne is a " + option1 + " and one is a " + option2 + ".")
+    print_pause("You wake up one morning and find you"
+                + " had an unexpected and unseen visitor.")
+    print_pause("On your desk there are two mysterious things." +
+                "\nOne is a " + option1 + " and one is a " + option2 + ".")
     time.sleep(3)
     print("Things are about to get curiouser and curiouser!!!")
     open_it(option1, option2)
-   
+
 
 # def to decide what to open
+
+
 def open_it(option1, option2):
-    print_pause("It is not every day that a " + option1 + " or a " + option2 + " turn up in your house.")
+    print_pause("It is not every day that a "
+                + option1 + " or a " + option2 +
+                " turn up in your house.")
     print_pause("Do you you dare to open a mysterious gift?")
     print_pause("Enter 1 to open the " + option1)
     print_pause("Enter 2 to open the " + option2)
@@ -56,13 +76,14 @@ def open_it(option1, option2):
                 option = option2
                 foul_odor(option)
                 break
-        
 
 
 def foul_odor(option):
     print_pause("Oh the vile air!")
-    print_pause("Touching the " + option + " unleashed an unimaginable stink! ")
-    print_pause("The only thing that can save you now is to use your time turner and start again")
+    print_pause("Touching the " + option
+                + " unleashed an unimaginable stink! ")
+    print_pause("The only thing that can save you " +
+                "now is to use your time turner and start again")
     print_pause("Enter 1 to grab your time turner and try again.")
     print_pause("Enter 2 to sucuumb to the foul stench.")
     while True:
@@ -79,10 +100,12 @@ def foul_odor(option):
         else:
             print("Invalid entry, try again")
 
+
 def glimmering_treasures(option):
     print_pause("What a glorious sight!")
-    print_pause("Touching the " + option + " unleashed an avalanche of treasures! ")
-    print_pause("You are very rich! But do you do dare try again for more wealth?")
+    print_pause("Touching the " + option
+                + " unleashed an avalanche of treasures! ")
+    print_pause("You are very rich! But do you do dare try again?")
     print_pause("Enter 1 to grab your time turner and try again.")
     print_pause("Enter 2 to live happily off your riches.")
     while True:
@@ -93,16 +116,19 @@ def glimmering_treasures(option):
             play_game()
             break
         elif choice == "2":
-            print("You have chosen to be satisfied with your newfound glorious wealth.")
+            print("You have chosen to be satisfied " +
+                  "with your newfound glorious wealth.")
             game_over()
             break
-        else: 
+        else:
             print("Invalid entry, try again")
-  
+
+
 def game_over():
     print_pause("GAME OVER")
     print_pause("Please play again!")
 
 # Run game on load
-play_game()
 
+
+play_game()
