c7d7d2bd93071e1b9424f2e161a3c04e4b5b426a
Tamar <mischegoss@gmail.com>
2020-03-26 21:16:50 -0400

    Basic choices working

--- adventure-game.py
+++ adventure-game.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 # This is the print_pause helper function as in the instructions
 
@@ -10,16 +11,33 @@ def print_pause(my_string):
 
 def play_game() :
     option1 = random.choice(["red box", "blue box", "basket of puppies", "golden box with no key",
-                            "an old book", "a Marauder's map", "a tiny black box", "a jar of something foul but glimmering"])
-    option2 = random.choice(["old key", "purple box with feathers", "an invitation", "a mysterious envelope",
-                            "an old pirate's map", "a box that says 'do not open'", "a magical pumpkin", "a sorting hat"])
+                            "very old book", "Marauder's map", "tiny black box", "jar of something foul but glimmering"])
+    option2 = random.choice(["old key", "purple box with feathers", "invitation from a witch", "mysterious envelope",
+                            "very old pirate's map", "box that says 'do not open'", "magical pumpkin", "a sorting hat"])
+    intro(option1, option2)
+    
+
+def intro(option1, option2):
+    print_pause("You wake up one morning and find you had an unexpected and unseen visitor.")   
+    print_pause("On your desk there are two mysterious things. \n One is a " + option1 + " and one is a " + option2 + ".")
+    time.sleep(4)
+    print("Things are about to get curiouser and curiouser!!!")
+    open_it(option1, option2)
 
 
-def intro():
-    print_pause("You wake up one morning and find you had an unexpected visitor.")   
-    print_pause("On your desk there are two mysterious things.  one is blue and one is red.")
-    input("Press 1 to open the red package. Press 2 to to open the blue package")
+def open_it(option1, option2):
+    print_pause("It is not every day that a " + option1 + option2 + "turn up in your house.")
+    print_pause("Do you you dare to open a mysterious gift?")
+    print_pause("Enter 1 to open the " + option1)
+    print_pause("Enter 1 to open the " + option2)
+    choice = ""
+    while choice not in ['1', '2']:
+        choice = input("(Please enter 1 or 2.)\n")
 
 
 
-intro(option1, option2)
+
+play_game()
+
+
+input("Press 1 to open the red package. Press 2 to to open the blue package")
